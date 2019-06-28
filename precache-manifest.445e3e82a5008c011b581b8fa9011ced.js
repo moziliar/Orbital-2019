8fbceb658785d580df3a17817b172286f0cfee02
mo <mzongran@comp.nus.edu.sg>
2019-06-28 10:06:23 +0800

    Updates

--- precache-manifest.445e3e82a5008c011b581b8fa9011ced.js
+++ precache-manifest.445e3e82a5008c011b581b8fa9011ced.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "934f7fd1ea05873a558a9b34ad803554",
+    "revision": "8e4b2d3c93b211b7bfa22b6a5d276a93",
     "url": "/Orbital-2019/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Orbital-2019/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "243a0abf5642e1bd5672",
+    "revision": "ca31b8f624f7670ceaf6",
     "url": "/Orbital-2019/static/css/main.135fa24f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Orbital-2019/static/js/2.05defc7c.chunk.js"
   },
   {
-    "revision": "243a0abf5642e1bd5672",
-    "url": "/Orbital-2019/static/js/main.d0d2571d.chunk.js"
+    "revision": "ca31b8f624f7670ceaf6",
+    "url": "/Orbital-2019/static/js/main.85cd8bd1.chunk.js"
   },
   {
     "revision": "a7096b550302eeb1b00c",
