bc162e53ab05d4168ae45e08527aff4c7a0a9d02
mo <mzongran@comp.nus.edu.sg>
2019-06-28 09:47:41 +0800

    Rerouting

--- build/precache-manifest.857af47682fa316aa455323d28132a37.js
+++ build/precache-manifest.857af47682fa316aa455323d28132a37.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eb719128f37b53438c37a647fbc66bfe",
+    "revision": "a952a662208f1ee2eaee98e4b12bbb3c",
     "url": "/Orbital-2019/index.html"
   },
   {
-    "revision": "d4794220e2e1378fe6c1",
+    "revision": "2f436e1707e87f79f777",
     "url": "/Orbital-2019/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "5c6c0f977ddd46b1f8d2",
+    "revision": "332cd3ec9a68bc371d66",
     "url": "/Orbital-2019/static/css/main.135fa24f.chunk.css"
   },
   {
-    "revision": "d4794220e2e1378fe6c1",
-    "url": "/Orbital-2019/static/js/2.e09b1c96.chunk.js"
+    "revision": "2f436e1707e87f79f777",
+    "url": "/Orbital-2019/static/js/2.05defc7c.chunk.js"
   },
   {
-    "revision": "5c6c0f977ddd46b1f8d2",
-    "url": "/Orbital-2019/static/js/main.345b2212.chunk.js"
+    "revision": "332cd3ec9a68bc371d66",
+    "url": "/Orbital-2019/static/js/main.d738159c.chunk.js"
   },
   {
     "revision": "a7096b550302eeb1b00c",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Orbital-2019/precache-manifest.7014184dcd13ceaff6e8ea210862dd07.js"
+  "/Orbital-2019/precache-manifest.857af47682fa316aa455323d28132a37.js"
 );
 
 self.addEventListener('message', (event) => {
